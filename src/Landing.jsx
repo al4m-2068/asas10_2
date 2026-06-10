c22b31c05d19bff087ac1b524c1be6b973f7bfa0
Albar Malik <albar2806@student.abudzar.sch.id>
2026-06-10 09:42:34 +0700

    done touching

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,27 +3,33 @@ import './index.css'
 export default function Landing() {
   return (
     <>
-      <header className='px-10 py-5'>
+      <header className='px-10 py-5 flex justify-between items-center'>
         <h1 className='text-lg font-bold'>abdl<span className='text-amber-400'>.</span>mlk</h1>
+        <ul className='flex gap-5 text-xs text-zinc-400 uppercase *:hover:text-amber-400 *:transition-all *:duration-150'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Projects</a></li>
+        </ul>
       </header>
       <section className='flex flex-col gap-10 items-center justify-center text-center h-full'>
         <img src="/src/assets/me.jpg" className='absolute size-50 object-cover object-bottom mb-25 rounded-tl-[70px] rounded-tr-[130px] rounded-br-2xl rounded-bl-[120px]' alt="" />
 
-        <h1 className='px-10 flex flex-col w-120 items-center justify-center text-[20vh]/[90%] tracking-tighter font-bold'>
-          <span className="bg-transparent group w-1/3 flex justify-center gap-1 hover:font-bold transition-all duration-300 *:transition-all *:duration-300">
-            <p className="-translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300">{'<'}</p>
+        <h1 className='px-10 flex flex-col w-120 items-center justify-center text-[20vh]/[90%] tracking-tighter font-bold *:transition-all *:duration-190'>
+          <span className="bg-transparent group w-1/3 flex items-center justify-center gap-7 hover:font-bold *:transition-all *:duration-50">
+            <p className="group-hover:text-amber-400 text-7xl opacity-0 group-hover:opacity-100">{'<'}</p>
               Albar
-            <p className="translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300">{'>'}</p>
+            <p className=" group-hover:text-amber-400 text-7xl opacity-0 group-hover:opacity-100">{'>'}</p>
           </span>
-          <span className="bg-transparent group w-1/3 flex justify-center gap-1 hover:font-bold transition-all duration-300 *:transition-all *:duration-300">
-            <p className="-translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300">{'<'}</p>
+          <span className="bg-transparent group w-1/3 flex items-center justify-center gap-7 hover:font-bold *:transition-all *:duration-50">
+            <p className="group-hover:text-amber-400 text-7xl opacity-0 group-hover:opacity-100">{'<'}</p>
               Abdul
-            <p className="translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300">{'>'}</p>
+            <p className=" group-hover:text-amber-400 text-7xl opacity-0 group-hover:opacity-100">{'>'}</p>
           </span>
-          <span className="bg-transparent group w-1/3 flex justify-center gap-1 hover:font-bold transition-all duration-300 *:transition-all *:duration-300">
-            <p className="-translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300">{'<'}</p>
+          <span className="bg-transparent group w-1/3 flex items-center justify-center gap-7 hover:font-bold *:transition-all *:duration-50">
+            <p className="group-hover:text-amber-400 text-7xl opacity-0 group-hover:opacity-100">{'<'}</p>
               Malik
-            <p className="translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300">{'>'}</p>
+            <p className=" group-hover:text-amber-400 text-7xl opacity-0 group-hover:opacity-100">{'>'}</p>
           </span>
         </h1>
 
@@ -38,10 +44,16 @@ export default function Landing() {
           </button>
         </div>
       </section>
-      <footer className='px-10 py-5 flex justify-between text-sm uppercase'>
-        <p className='flex items-center'>Copy<span className='group flex flex-col h-6 overflow-hidden'><span>right</span><span>wrong</span></span> 2025 &copy; byme</p>
+      <footer className='px-10 py-5 flex justify-between text-xs uppercase text-zinc-400'>
+        <p className='flex items-center '>Copy
+          <span className='group flex flex-col h-4 overflow-hidden *:transition-all *:duration-150'>
+            <span className='group-hover:-translate-y-full'>right</span>
+            <span className='group-hover:-translate-y-full'>wrong</span>
+          </span>
+          2025 &copy; byme.fr
+        </p>
         
-        <ul className='flex items-center gap-10'>
+        <ul className='flex items-center gap-5 *:hover:text-amber-400 *:transition-all *:duration-150'>
           <li><a href="#">GH</a></li>
           <li><a href="#">LN</a></li>
           <li><a href="#">IG</a></li>
